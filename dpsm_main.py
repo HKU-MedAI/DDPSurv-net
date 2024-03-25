6a6a60bed404cbf7f68da076cd86a5c1909bbd4d
xjc <xjc1022@connect.hku.hk>
2024-03-25 21:30:37 +0800

    update

---
+++ dpsm_main.py
@@ -0,0 +1,83 @@
+from auton_survival import datasets
+
+outcomes, features = datasets.load_support()
+
+# %%
+
+from auton_survival.preprocessing import Preprocessor
+
+cat_feats = ['sex', 'dzgroup', 'dzclass', 'income', 'race', 'ca']
+num_feats = ['age', 'num.co', 'meanbp', 'wblc', 'hrt', 'resp',
+             'temp', 'pafi', 'alb', 'bili', 'crea', 'sod', 'ph',
+             'glucose', 'bun', 'urine', 'adlp', 'adls']
+
+features = Preprocessor().fit_transform(features, cat_feats=cat_feats, num_feats=num_feats)
+
+import numpy as np
+
+horizons = [0.25, 0.5, 0.75]
+times = np.quantile(outcomes.time[outcomes.event == 1], horizons).tolist()
+
+x, t, e = features.values.astype(float), outcomes.time.values.astype(float), outcomes.event.values.astype(float)
+
+n = len(x)
+
+tr_size = int(n * 0.70)
+vl_size = int(n * 0.10)
+te_size = int(n * 0.20)
+
+x_train, x_test, x_val = x[:tr_size], x[-te_size:], x[tr_size:tr_size + vl_size]
+t_train, t_test, t_val = t[:tr_size], t[-te_size:], t[tr_size:tr_size + vl_size]
+e_train, e_test, e_val = e[:tr_size], e[-te_size:], e[tr_size:tr_size + vl_size]
+
+models = []
+
+from auton_survival.models.dpsm import DeepDP
+from sklearn.model_selection import ParameterGrid
+
+param_grid = {'k' : [10],
+              'distribution' : ['LogNormal'],
+              'learning_rate' : [ 1e-4, 1e-3],
+              'layers' : [ [], [100], [100, 100] ]
+             }
+
+params = ParameterGrid(param_grid)
+
+models = []
+for param in params:
+    model = DeepDP(k = param['k'],
+                                 distribution = param['distribution'],
+                                 layers = param['layers'])
+    # The fit method is called to train the model
+    model.fit(x_train, t_train, e_train, iters = 100, learning_rate = param['learning_rate'])
+    models.append([[model.compute_nll(x_val, t_val, e_val), model]])
+
+best_model = min(models)
+model = best_model[0][1]
+
+out_risk = model.predict_risk(x_test, times)
+out_survival = model.predict_survival(x_test, times)
+
+from sksurv.metrics import concordance_index_ipcw, brier_score, cumulative_dynamic_auc
+
+cis = []
+brs = []
+
+et_train = np.array([(e_train[i], t_train[i]) for i in range(len(e_train))],
+                 dtype = [('e', bool), ('t', float)])
+et_test = np.array([(e_test[i], t_test[i]) for i in range(len(e_test))],
+                 dtype = [('e', bool), ('t', float)])
+et_val = np.array([(e_val[i], t_val[i]) for i in range(len(e_val))],
+                 dtype = [('e', bool), ('t', float)])
+
+for i, _ in enumerate(times):
+    cis.append(concordance_index_ipcw(et_train, et_test, out_risk[:, i], times[i])[0])
+brs.append(brier_score(et_train, et_test, out_survival, times)[1])
+roc_auc = []
+for i, _ in enumerate(times):
+    roc_auc.append(cumulative_dynamic_auc(et_train, et_test, out_risk[:, i], times[i])[0])
+for horizon in enumerate(horizons):
+    print(f"For {horizon[1]} quantile")
+    print("TD Concordance Index:", cis[horizon[0]])
+    print("Brier Score:", brs[0][horizon[0]])
+    print("ROC AUC ", roc_auc[horizon[0]][0], "\n")
---
+++ tests/kkbox.py
@@ -0,0 +1,105 @@
+from pycox.datasets import from_kkbox
+
+kkbox_data = from_kkbox._DatasetKKBoxChurn()
+#kkbox_data.download_kkbox()
+
+df = kkbox_data.read_df()
+
+import numpy as np
+import pandas as pd
+
+e = np.array(df.event)
+t = np.array(df.duration)
+x = df.drop(columns=['event','duration','msno'])
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+x['gender'] = le.fit_transform(x['gender'])
+x['registered_via'] = le.fit_transform(x['registered_via'])
+x['city'] = le.fit_transform(x['city'])
+x = np.array(x).astype(float)
+
+import os, sys
+import numpy as np 
+
+path = '/home/r10user10/Documents/Jiacheng/dspm-auton-survival'
+os.chdir(path)
+
+from auton_survival import datasets
+
+n = len(x)
+
+tr_size = int(n * 0.70)
+vl_size = int(n * 0.10)
+te_size = int(n * 0.20)
+
+print(tr_size, vl_size, te_size)
+
+x_train, x_test, x_val = x[:tr_size], x[-te_size:], x[tr_size:tr_size + vl_size]
+t_train, t_test, t_val = t[:tr_size], t[-te_size:], t[tr_size:tr_size + vl_size]
+e_train, e_test, e_val = e[:tr_size], e[-te_size:], e[tr_size:tr_size + vl_size]
+
+models = []
+
+from auton_survival.models.dpsm import DeepDP
+from auton_survival.models.dsm import DeepSurvivalMachines
+
+# model = DeepDP(k=3,
+#                distribution='LogNormal',
+#                layers=[100])
+model = DeepSurvivalMachines(
+    k=4,
+    distribution="LogNormal",
+    # distribution="Weibull",
+    layers=[100]
+)
+# The fit method is called to train the model
+model.fit(x_train, t_train, e_train, iters=1, learning_rate=0.001)
+
+trained_weights = model.trained_weights
+print(trained_weights.shape)
+
+horizons = [0.25, 0.5, 0.75]
+times = np.quantile(t[e == 1], horizons).tolist()
+out_risk = model.predict_risk(x_test, times)
+out_survival = model.predict_survival(x_test, times)
+
+from sksurv.metrics import concordance_index_ipcw, brier_score, cumulative_dynamic_auc
+
+cis = []
+brs = []
+
+et_train = np.array([(e_train[i], t_train[i]) for i in range(len(e_train))],
+                 dtype = [('e', bool), ('t', float)])
+et_test = np.array([(e_test[i], t_test[i]) for i in range(len(e_test))],
+                 dtype = [('e', bool), ('t', float)])
+et_val = np.array([(e_val[i], t_val[i]) for i in range(len(e_val))],
+                 dtype = [('e', bool), ('t', float)])
+
+for i, _ in enumerate(times):
+    cis.append(concordance_index_ipcw(et_train, et_test, out_risk[:, i], times[i])[0])
+brs.append(brier_score(et_train, et_test, out_survival, times)[1])
+roc_auc = []
+for i, _ in enumerate(times):
+    roc_auc.append(cumulative_dynamic_auc(et_train, et_test, out_risk[:, i], times[i])[0])
+for horizon in enumerate(horizons):
+    print(f"For {horizon[1]} quantile")
+    print("TD Concordance Index:", cis[horizon[0]])
+    print("Brier Score:", brs[0][horizon[0]])
+    print("ROC AUC ", roc_auc[horizon[0]][0], "\n")
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+figs, axes = plt.subplots(1, 3, figsize=(15, 5))
+iter_idx = [24, 25, 26]
+
+for idx in range(3):
+    sns.kdeplot(trained_weights[iter_idx[idx]][:, 0], fill=True, ax=axes[idx])
+    sns.kdeplot(trained_weights[iter_idx[idx]][:, 1], fill=True, ax=axes[idx])
+    sns.kdeplot(trained_weights[iter_idx[idx]][:, 2], fill=True, ax=axes[idx])
+    sns.kdeplot(trained_weights[iter_idx[idx]][:, 3], fill=True, ax=axes[idx])
+    axes[idx].set_title(f'Iter {iter_idx[idx]}')
+    axes[idx].set_xlim(0.249, 0.251)
